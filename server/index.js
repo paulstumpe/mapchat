b7e5adaf03f4f0181fbc3d8241c2c7f5dbfcb60a
Jeff <jeff.l.watkins2@gmail.com>
2020-01-22 14:32:31 -0600

    added googlePlaces to index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const db = require('./database/index');
 const app = express();
 const port = 8080;
 const { apiRouter } = require('./api/routes');
+require('./services/googlePlaces');
 
 app.use('/', apiRouter);
 app.get('/', (req, res) => res.send('Hello World!'));
