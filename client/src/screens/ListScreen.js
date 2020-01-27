5c4ba65bbd2bf804195845f17293bae8813f5ecf
Jeff <jeff.l.watkins2@gmail.com>
2020-01-27 15:43:11 -0600

    Modifies ListScreen to use cards

--- client/src/screens/ListScreen.js
+++ client/src/screens/ListScreen.js
@@ -1,20 +1,41 @@
 import * as React from 'react';
-import {
-  List,
-  Subheading,
-  Paragraph,
-} from 'react-native-paper';
+import { Avatar, Button, Card, Title, List, Subheading, Paragraph } from 'react-native-paper';
+
 const blah ={text:"lol",username:"user", timestamp:"later", description:"lol"};
 const array = [blah,blah,blah];
-export default function ListScreen({screenProps}){
-    return ( 
-      <List.Section>
-      {array.map(post=>(<List.Section >
-          < Subheading >{post.text}</Subheading>
-            < Paragraph >{post.username}</Paragraph>
-            < Paragraph >{post.timestamp}</Paragraph>
-            < Paragraph >{post.description}</Paragraph>
-      </List.Section>))}
-      </List.Section>
-    );
-}
+
+// export default function ListScreen({screenProps}){
+//     return ( 
+//       <List.Section>
+//       {array.map(post=>(<List.Section >
+//           < Subheading >{post.text}</Subheading>
+//             < Paragraph >{post.username}</Paragraph>
+//             < Paragraph >{post.timestamp}</Paragraph>
+//             < Paragraph >{post.description}</Paragraph>
+//       </List.Section>))}
+//       </List.Section>
+//     );
+// }
+
+
+
+const ListScreen = () => (
+  <Card>
+    <Card.Title
+      title="Marc-y Marc & da Funky Bunch"
+      subtitle="Subtitle Text"
+      left={props => <Avatar.Icon {...props} icon="folder" />}
+    />
+    {/* <Card.Cover source={{ uri: "https://picsum.photos/350" }} /> */}
+    <Card.Content>
+      <Title>Test Message</Title>
+      <Paragraph>Cue the Lorem Ipsum</Paragraph>
+    </Card.Content>
+    <Card.Actions>
+      <Button>Confirm</Button>
+      <Button>Cancel</Button>
+    </Card.Actions>
+  </Card>
+);
+
+export default ListScreen;
