3f07baa616b41a2ffacc0d9fc74af15df9b756a2
Jeff <jeff.l.watkins2@gmail.com>
2020-01-28 16:53:46 -0600

    map data from database to listscreen

--- client/src/screens/ListScreen.js
+++ client/src/screens/ListScreen.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { Avatar, Button, Card, Title, List, Subheading, Paragraph } from 'react-native-paper';
 import { ScrollView } from 'react-native-gesture-handler';
+import { getAll } from '../Helper'
 
 const blah ={text:"lol",username:"user", timestamp:"later", description:"lol"};
 const array = [blah,blah,blah,blah,blah];
@@ -19,10 +21,27 @@ const array = [blah,blah,blah,blah,blah];
 // }
 
 // Get yourself an axios request so that you can work with some real data instead of dummy data
-const ListScreen = ({screenProps}) => (
+const ListScreen = ({screenProps}) => {
+  const [messages, setMessages] = useState([]);
+  getAll()
+    .then(({
+      data
+    }) => {
+      console.log(data);
+      const allMessages = data.map((message) => {
+        message.longitude = parseFloat(message.coordinate.long);
+        message.latitude = parseFloat(message.coordinate.lat);
+        return message;
+      })
+      console.log(typeof allMessages[0].latitude);
+      setMessages(allMessages);
+    })
+    .catch(err => console.log(err));
+
+  return (
 <List.Section>
   <ScrollView>
-  {array.map(post=> (
+  {messages.map(post=> (
   <Card>
     <Card.Title
       title={post.username}
@@ -42,6 +61,7 @@ const ListScreen = ({screenProps}) => (
   ))}
   </ScrollView>
 </List.Section>
-);
+  )
+};
 
 export default ListScreen;
