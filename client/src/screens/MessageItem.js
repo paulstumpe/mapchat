ec4008d484d9c7329a33b3aa5a438def1b73e5c6
Jeff <jeff.l.watkins2@gmail.com>
2020-01-29 12:05:46 -0600

    comment out tcomb to keep that was causing issues

--- client/src/screens/MessageItem.js
+++ client/src/screens/MessageItem.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, ScrollView, Text, StyleSheet, TouchableHighlight, } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
-import t from 'tcomb-form-native'; // 0.6.9
+// import t from 'tcomb-form-native'; // 0.6.9
 // import * as React from 'react';
 import { List } from 'react-native-paper';
 
