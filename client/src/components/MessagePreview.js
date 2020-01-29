4959cbaedf7c8fc912acc653e41f18aeba7f9332
Your marcdwest32 <marcdwest@gmail.com>
2020-01-29 15:27:53 -0600

    creates messagePreview to be rendered by ListScreen

---
+++ client/src/components/MessagePreview.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Avatar, Card, Divider, Paragraph } from 'react-native-paper';
+
+const MessagePreview = ({ messages }) =>
+  messages &&
+  messages.map((message, i) => {
+    console.log(message);
+    return (
+      <Card style={styles.container} key={i}>
+        <Card.Title
+          title={message.title}
+          subtitle={message.username}
+          left={() => <Avatar.Text size={36} label={message.username[0]} />}
+        />
+        <Divider />
+        <Card.Content style={{ paddingTop: 10 }}>
+          <Paragraph>{message.text}</Paragraph>
+        </Card.Content>
+        <Divider />
+      </Card>
+    );
+  });
+
+MessagePreview.navigationOptions = {
+  title: 'title',
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    paddingBottom: 10,
+    backgroundColor: '#fff',
+  },
+  name: {
+    fontWeight: 'bold',
+    color: 'blue',
+  },
+});
+
+export default MessagePreview;
