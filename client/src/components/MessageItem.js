b0752c012d758e7524539f73e3cfbd32c40af750
Your marcdwest32 <marcdwest@gmail.com>
2020-01-29 09:06:03 -0600

    sets up scroll view

--- client/src/components/MessageItem.js
+++ client/src/components/MessageItem.js
@@ -1,5 +1,11 @@
 import React, { Component, useState } from 'react';
-import { StyleSheet, View, KeyboardAvoidingView } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  KeyboardAvoidingView,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native';
 import {
   Avatar,
   Button,
@@ -22,78 +28,81 @@ const comments = [
   { username: 'Bender', text: 'Bite my shiny metal ass' },
   { username: 'Professor', text: 'Good news everyone' },
   { username: 'Leela', text: `That isn't a real name, Fry` },
+  { username: 'Bender', text: 'Bite my shiny metal ass' },
+  { username: 'Professor', text: 'Good news everyone' },
+  { username: 'Leela', text: `That isn't a real name, Fry` },
 ];
 
-const MessageItem = () => {
+const MessageItem = props => {
+  console.log(props.username);
   const [comment, setComment] = useState('');
   const postComment = () => {
-    // postMessageHelper({ title, message }, screenProps.coords)
-    //   .then(data => {
-    //     data;
-    //     console.log('success', data);
-    //   })
-    //   .catch(err => {
-    //     console.log(err);
-    //   });
     console.log(comment);
     setComment('');
   };
+
   return (
-    <KeyboardAvoidingView style={styles.container} behavior='position' enabled>
-      <Card>
-        <Card.Title
-          title={message.username}
-          subtitle={message.title}
-          left={props => <Avatar.Text size={48} label='PJF' />}
-        />
-        <Divider />
-        <Card.Content style={{ paddingTop: 10 }}>
-          <Paragraph>{message.text}</Paragraph>
-        </Card.Content>
-      </Card>
-      {comments &&
-        comments.map(comment => (
-          <Card style={{ marginTop: 10 }}>
-            <Subheading> {comment.username}</Subheading>
+    <Card style={styles.container}>
+      <ScrollView>
+        <KeyboardAvoidingView behavior='position' enabled>
+          <Card>
+            <Card.Title
+              title={message.username}
+              subtitle={message.title}
+              left={() => <Avatar.Text size={48} label='PJF' />}
+            />
             <Divider />
             <Card.Content style={{ paddingTop: 10 }}>
-              <Paragraph>{comment.text}</Paragraph>
+              <Paragraph>{message.text}</Paragraph>
             </Card.Content>
           </Card>
-        ))}
-      <Card
-        style={{
-          borderRadius: 10,
-          position: 'relative',
-          zIndex: 2,
-          marginTop: 10,
-        }}
-      >
-        <Card.Content>
-          <TextInput
-            label='Comment'
-            placeholder='comment'
-            mode='outlined'
-            multiline={true}
-            numberOfLines={3}
-            value={comment}
-            onChangeText={comment => setComment(comment)}
-          />
-          <Button
-            icon='send'
-            mode='contained'
+          {comments &&
+            comments.map((comment, i) => {
+              return (
+                <Card style={{ marginTop: 10 }} key={i}>
+                  <Subheading> {comment.username}</Subheading>
+                  <Divider />
+                  <Card.Content style={{ paddingTop: 10 }}>
+                    <Paragraph>{comment.text}</Paragraph>
+                  </Card.Content>
+                </Card>
+              );
+            })}
+          <Card
             style={{
+              borderRadius: 10,
+              position: 'relative',
+              zIndex: 2,
               marginTop: 10,
-              marginRight: 220,
-              height: 40,
             }}
-            onPress={() => postComment()}
           >
-            Comment
-          </Button>
-        </Card.Content>
-      </Card>
-    </KeyboardAvoidingView>
+            <Card.Content>
+              <TextInput
+                label='Comment'
+                placeholder='comment'
+                mode='outlined'
+                multiline={true}
+                numberOfLines={3}
+                value={comment}
+                onChangeText={comment => setComment(comment)}
+              />
+              <Button
+                icon='send'
+                mode='contained'
+                style={{
+                  marginTop: 10,
+                  marginRight: 220,
+                  height: 40,
+                }}
+                onPress={() => postComment()}
+              >
+                Comment
+              </Button>
+            </Card.Content>
+          </Card>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </Card>
   );
 };
 
@@ -105,6 +114,7 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 10,
     padding: 20,
+    paddingBottom: 10,
     backgroundColor: '#fff',
   },
   name: {
