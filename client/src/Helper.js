491a8599fc228e67442ea4d5f6d12cc3bf10ffcb
Your marcdwest32 <marcdwest@gmail.com>
2020-01-30 20:32:45 -0600

    Paul's helpers

--- client/src/Helper.js
+++ client/src/Helper.js
@@ -1,35 +1,37 @@
 import * as axios from 'axios';
+
 const localIP = '172.24.6.51';
-export let getAll = () => {
+
+export const getAll = () => {
   return axios.get(`http://${localIP}:8080/messages/all`);
 };
+
 export const getOne = postProperties => {
-  return axios.get(`http://${localIP}:8080/messages`);
+  return axios.get(`http://${localIP}:8080/messages`, postProperties);
 };
-export const postMessageHelper = (postInput, location) => {
-  console.log(location);
+
+export const postMessageHelper = (postInput, location, userId) => {
+  console.log(postInput.global);
   let body = {
     title: postInput.title,
     text: postInput.message,
-    // post_public,
-    // post_local,
-    // time_created,
-    // updated_at,
-    // time_expires,
-    // post_anonymous,
+    post_public: postInput.comments,
+    post_local: postInput.global,
+    post_anonymous: postInput.anon,
     coordinate: {
       long: location.longitude,
       lat: location.latitude,
     },
+    userId: userId,
   };
   return axios.post(`http://${localIP}:8080/messages`, body);
 };
 
-
-export const getUser = (userInput)=>{
+export const getUser = userInput => {
   return axios.get(`http://${localIP}:8080/users`, userInput);
-}
-export const postUser = (userInput)=>{
+};
+
+export const postUser = userInput => {
   let body = {
     username: userInput.username,
     name_first: userInput.name_first,
@@ -39,32 +41,31 @@ export const postUser = (userInput)=>{
     bio: userInput.bio,
     status: userInput.status,
     public: userInput.public,
-  }
-  return axios.post(`http://${localIP}:8080/users`, body);
-}
-
-
-export const postComment = (commentInput) => {
-  const body = {
-    postId:commentInput.postId, 
-    text: commentInput.text, 
-    userId: commentInput.userId
-  }
-  return axios.post(`http://${localIP}:8080/comments`, body);
-}
-
-
-export const addLike = (likeInput)=>{
-  const body = {
-    userId: likeInput.userId,
-    postId: likeInput.postId
-  }
-  return axios.post(`http://${localIP}:8080/likes`, body);
-}
-export const removeLike = (likeInput)=>{
-  const body = {
-    userId: likeInput.userId,
-    postId: likeInput.postId
   };
-  return axios.delete(`http://${localIP}:8080/likes`, {data:body});
-}
+  return axios.post(`http://${localIP}:8080/users`, body);
+};
+
+export const postComment = commentInput => {
+  const body = {
+    postId: commentInput.postId,
+    text: commentInput.text,
+    userId: commentInput.userId,
+  };
+  return axios.post(`http://${localIP}:8080/comments`, body);
+};
+
+export const addLike = likeInput => {
+  const body = {
+    userId: likeInput.userId,
+    postId: likeInput.postId,
+  };
+  return axios.post(`http://${localIP}:8080/likes`, body);
+};
+
+export const removeLike = likeInput => {
+  const body = {
+    userId: likeInput.userId,
+    postId: likeInput.postId,
+  };
+  return axios.delete(`http://${localIP}:8080/likes`, { data: body });
+};
