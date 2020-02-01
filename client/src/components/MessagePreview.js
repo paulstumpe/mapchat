4f708182a708d03bd2fdd031d49f6ea65e23a233
Your marcdwest32 <marcdwest@gmail.com>
2020-01-31 19:37:42 -0600

    import Profile as modal and set up toggle features

--- client/src/components/MessagePreview.js
+++ client/src/components/MessagePreview.js
@@ -1,32 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Avatar, Card } from 'react-native-paper';
+import Modal from 'react-native-modal';
 import MessageItem from '../components/MessageItem';
+import Profile from '../components/Profile';
 
-const MessagePreview = ({ messages }) =>
-  messages &&
-  messages.map((message, i) => {
-    console.log(message);
-    return (
-      <Card style={message.post_local ? styles.local : styles.global} key={i}>
-        <Card.Title
-          title={message.title}
-          subtitle={message.user ? message.user.username : message.username}
-          left={() => (
-            <Avatar.Text
-              style={styles.avatar}
-              color='#2B4162'
-              size={48}
-              label={
-                message.user ? message.user.username[0] : message.username[0]
-              }
-            />
-          )}
-        />
-        <MessageItem post={message} />
-      </Card>
-    );
-  });
+const MessagePreview = ({ messages }) => {
+  const [profileModal, toggleProfileModal] = useState(false);
+
+  return (
+    messages &&
+    messages.map((message, i) => {
+      console.log(message);
+      return (
+        <Card
+          style={message.post_local ? styles.local : styles.global}
+          onPress={() => toggleProfileModal(true)}
+          key={i}
+        >
+          <Card.Title
+            title={message.title}
+            subtitle={message.user ? message.user.username : message.username}
+            left={() => (
+              <Avatar.Text
+                style={styles.avatar}
+                color='#2B4162'
+                size={48}
+                label={
+                  message.user ? message.user.username[0] : message.username[0]
+                }
+              />
+            )}
+          />
+          <MessageItem post={message} />
+          <Modal
+            isVisible={profileModal}
+            onBackButtonPress={() => toggleProfileModal(false)}
+          >
+            <Profile />
+          </Modal>
+        </Card>
+      );
+    })
+  );
+};
 
 MessagePreview.navigationOptions = {
   title: 'title',
