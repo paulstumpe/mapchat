4403dcf98461cc960d0447f9a47855b8b582428b
Your marcdwest32 <marcdwest@gmail.com>
2020-02-04 14:07:23 -0600

    formatting

--- client/src/screens/ListScreen.js
+++ client/src/screens/ListScreen.js
@@ -2,8 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { ScrollView } from 'react-native';
 import MessagePreview from '../components/MessagePreview';
 import { getAll } from '../Helper';
-import { useFocusEffect } from 'react-navigation-hooks'
-
+import { useFocusEffect } from 'react-navigation-hooks';
 
 const ListScreen = screenProps => {
   const [messages, setMessages] = useState([]);
@@ -15,18 +14,19 @@ const ListScreen = screenProps => {
     });
   }, []);
 
-  useFocusEffect(useCallback(() => {
-    console.debug("screen takes focus");
-    getAll().then(response => {
-      const allPosts = response.data;
-      setMessages(allPosts);
-    });
-    //component did unmount
-    return () => {
-
-      console.debug("screen loses focus")
-    };
-  }, []));
+  useFocusEffect(
+    useCallback(() => {
+      console.debug('screen takes focus');
+      getAll().then(response => {
+        const allPosts = response.data;
+        setMessages(allPosts);
+      });
+      //component did unmount
+      return () => {
+        console.debug('screen loses focus');
+      };
+    }, []),
+  );
 
   return (
     <ScrollView>
